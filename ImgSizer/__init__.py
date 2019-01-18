39c522c1ec613b7dec689f91092ac73fac6693d4
Tumurtogtokh <tdavaakhuu1@sheffield.ac.uk>
2019-01-18 21:19:53 +0000

    initial layout

---
+++ ImgSizer/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+# IMPORT local
+from .controller.thumbnail_maker import ThumbnailMakerService
---
+++ setup.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+setup(name='Image Re-Sizer', version='0.1', packages=find_packages())
