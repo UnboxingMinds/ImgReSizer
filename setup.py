f4492516e874386a3d4f00ac953ece5ec39ae004
Tumurtogtokh <tdavaakhuu1@sheffield.ac.uk>
2019-03-13 15:34:21 +0000

    setup ready to publish

--- setup.py
+++ setup.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-setup(name='Image Re-Sizer', version='0.1', packages=find_packages())
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["requests>=2", "Pillow>=5.4", "imagesize>=1.1"]
+# with open("requirements.txt") as req_files:
+    # requirements = req_files.read().splitlines()
+
+setup(
+    name="ImgReSizer",
+    version="0.0.1",
+    author="Tumurtogtokh Davaakhuu",
+    author_email="tumurtogtokh@gmail.com",
+    description="A package to re-size images",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UnboxingMinds/ImgReSizer",
+    packages=find_packages(),
+    # install_requires=parse_requirements('requirements.txt', session='hack'),
+    install_requires = requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: MIT License",
+    ],
+)
