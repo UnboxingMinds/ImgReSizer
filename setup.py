f9c3827cd08b6b1c2ae72c225c97fd75d9984568
Tumurtogtokh <tdavaakhuu1@sheffield.ac.uk>
2019-03-23 01:53:28 +0000

    add tests

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["requests>=2", "Pillow>=5.4"]
+requirements = ["requests>=2", "Pillow>=5.4", "imagesize>=1.1"]
 
 setup(
     name="imgresizer",
